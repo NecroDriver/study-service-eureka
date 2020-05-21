96d1f7485f324a75bb4d7c200e04ca0ee982eed2
NecroDriver <necroDriver@foxmail.com>
2020-05-21 17:28:27 +0800

    first commit

---
+++ src/main/java/com/xin/eureka/StudyServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.xin.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyServiceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyServiceEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xin/eureka/StudyServiceEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xin.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyServiceEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
